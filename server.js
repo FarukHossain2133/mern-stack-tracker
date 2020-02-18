bbd3d108619ff93c4dfbbb75ceb8027f5ee6a3b2
faruk <fa905432@gmail.com>
2020-02-18 15:09:42 +0600

    fixed package.json file

--- server.js
+++ server.js
@@ -29,12 +29,14 @@ const usersRouter = require('./routes/users');
 app.use('/exercises', exercisesRouter);
 app.use('/users', usersRouter);
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + './client/build/index.html'));
-});
 
+//Serve static assets if in production
 if(process.env.NODE_ENV === 'production'){
-   app.use(express.static(path.join(__dirname + './client/build')));
+   app.use(express.static('client/build'));
+
+   app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+   })
 }
 
 app.listen(port, () => {
